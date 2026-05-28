38f94a6f5da53c0c6d0fd61d9815ee0d21cf2b34
ROHAN0865 <rohanpandey0864@gmail.com>
2026-05-28 14:12:45 +0530

    table of given number

---
+++ day1/table.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n,i,a;
+    cout<< "enter the number of table you want\n" ;
+    cin>>n;
+    for(i=1;i<=10;i++){
+       a= n*i;
+     cout << a <<"\n";
+    }
+}
