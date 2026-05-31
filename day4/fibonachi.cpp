100905c63fdbadecb85ff1a72cabd5c1875ec3ea
ROHAN0865 <rohanpandey0864@gmail.com>
2026-05-31 14:08:28 +0530

    fibo

---
+++ day4/fibonachi.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i=0,n,b=1,a=0,c;
+    cout<<"enter the term upto which u want to print fibonachi series ";
+    cin>>n;
+    cout<<a <<'\t' <<b<<"\t";
+    for(int i=0;i<n;i++){
+        c=a+b;
+        cout<<c <<"\t";
+        a=b;
+        b=c;
+    }
+}
