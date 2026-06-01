a9a6f1c38d30a108785c5bb7107594a86ace2020
ROHAN0865 <rohanpandey0864@gmail.com>
2026-06-01 13:13:42 +0530

    day5

--- day5/perfectno.cpp
+++ day5/perfectno.cpp
@@ -3,11 +3,10 @@ using namespace std;
 
 int main()
 {
+    // A perfect number is a positive integer that is exactly equal to the sum of its proper positive divisors
     int num, sum = 0;
-
     cout << "Enter a number : ";
     cin >> num;
-
     for (int i = 1; i < num; i++)
     {
         if (num % i == 0)
@@ -15,7 +14,6 @@ int main()
             sum += i;
         }
     }
-
     if (sum == num)
     {
         cout << "Perfect Number";
@@ -24,6 +22,5 @@ int main()
     {
         cout << "Not a Perfect Number";
     }
-
     return 0;
 }
