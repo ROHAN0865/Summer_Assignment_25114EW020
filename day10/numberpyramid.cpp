16445cb398d61d50688780c8d968abe0c6dfc50c
ROHAN0865 <rohanpandey0864@gmail.com>
2026-06-10 15:12:16 +0530

    DAY 10

--- day10/numberpyramid.cpp
+++ day10/numberpyramid.cpp
@@ -8,18 +8,15 @@ int main()
     cin >> n;
     for (int i = 1; i <= n; i++)
     {
-        // spaces
-        for (int j = 1; j <= n - i; j++)
+        for (int j= 1; j <= n - i; j++)
         {
-            cout << " ";
+            cout<<" ";
         }
-        // increasing numbers
-        for (int k = 1; k <= i; k++)
+        for (int k=1;k<=i;k++)
         {
             cout << k;
         }
-        // decreasing numbers
-        for (int l = i - 1; l >= 1; l--)
+        for (int l=i-1;l>=1;l--)
         {
             cout << l;
         }
