dd22aa5a352f207a849be79a7300b97d01c59fff
ROHAN PANDEY <rohanpandey0864@gmail.com>
2026-06-16 07:29:21 +0530

    Create removeduplicate.cpp
    remove duplicate

---
+++ day16/removeduplicate.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int* arr = new int[n];
+
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    for(int i = 0; i < n; i++) {
+        bool duplicate = false;
+
+        for(int j = 0; j < i; j++) {
+            if(arr[i] == arr[j]) {
+                duplicate = true;
+                break;
+            }
+        }
+
+        if(!duplicate)
+            cout << arr[i] << " ";
+    }
+
+    delete[] arr;
+    return 0;
+}
