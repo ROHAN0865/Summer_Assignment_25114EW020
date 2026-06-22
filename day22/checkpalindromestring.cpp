220d5c8d41791b69d80674b5b9e238cc2253cc72
ROHAN0865 <rohanpandey0864@gmail.com>
2026-06-22 13:34:20 +0530

    check palindrome of string

---
+++ day22/checkpalindromestring.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+void palindrome(char str[]) {
+    int len = 0;
+    while(str[len] != '\0') {
+        len++;
+    }
+    bool flag = true;
+    for(int i = 0; i < len / 2; i++) {
+        if(str[i] != str[len - 1 - i]) {
+            flag = false;
+            break;
+        }
+    }
+
+    if(flag)
+        cout << "Palindrome String";
+    else
+        cout << "Not Palindrome String";
+}
+
+int main() {
+    char str[100];
+    cout << "Enter string: ";
+    cin.getline(str, 100);
+    palindrome(str);
+    return 0;
+}
